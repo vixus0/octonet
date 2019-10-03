814acc2c87fd706426522726793a3051026b9e80
Anshul Sirur <vixus0@gmail.com>
2019-10-03 11:18:38 +0100

    Add query retrying and fix teams query

--- graph.rb
+++ graph.rb
@@ -44,15 +44,32 @@ class Graph
 
 private
 
-  def teams_query(vars = {})
-    result = Github::Client.query(Github::TeamsQuery, variables: vars.merge({ 'org': ORG }))
+  def query(q, vars, max_retries = 10)
+    tries = 0
 
-    if result.data.nil?
-      LOG.error("no result data")
-      LOG.error("result: #{result.inspect}")
-      abort
+    until tries == max_retries
+      result = Github::Client.query(q, variables: vars.merge({ 'org': ORG }))
+
+      if result.data.nil?
+        if result.errors.messages['data'][0].downcase.include?('bad gateway')
+          tries = tries + 1
+          LOG.warn("Got 502 Bad Gateway, retrying")
+          sleep(1)
+          next
+        end
+        break
+      end
+
+      return result
     end
 
+    LOG.error("no result data")
+    LOG.error("result: #{result.inspect}")
+    abort
+  end
+
+  def teams_query(vars = {})
+    result = query(Github::TeamsQuery, vars)
     teams = result.data&.organization&.teams
     
     unless teams.nil?
@@ -61,51 +78,55 @@ private
 
       teams.nodes.each do |team|
         LOG.info("team: #{team.slug}")
-        @nodes.add({ 'id': team.slug, 'group': 1 })
+        @nodes.add({ 'id' => team.slug, 'group' => 1 })
 
         team.members.edges.each do |edge|
           member = edge.node
           LOG.info("  member: #{member.login} '#{member.name}' #{edge.role}")
-          @nodes.add({ 'id': member.login, 'data': {'name': member.name}, 'group': 2 })
-          @links.push({ 'source': member.login, 'target': team.slug, 'label': edge.role })
+          @nodes.add({ 'id' => member.login, 'data' => {'name' => member.name}, 'group' => 2 })
+          @links.push({ 'source' => member.login, 'target' => team.slug, 'label' => edge.role })
         end
 
         team.repositories.edges.each do |edge|
-          repo = edge.node
-          LOG.info("  repo: #{repo.name} #{edge.permission}")
-          @nodes.add({ 'id': repo.name, 'group': 3 })
-          @links.push({ 'source': repo.name, 'target': team.slug, 'label': edge.permission })
+          unless edge.nil? # private repos are nil I think
+            repo = edge.node
+            LOG.info("  repo: #{repo.name} #{edge.permission}")
+            @nodes.add({ 'id' => repo.name, 'group' => 3 })
+            @links.push({ 'source' => repo.name, 'target' => team.slug, 'label' => edge.permission })
+          end
         end
 
         if team.repositories.page_info.has_next_page
-          repos_query({ 'teamSlug': team.slug, 'repoCursor': team.repositories.page_info.end_cursor })
+          repos_query({ teamSlug: team.slug, repoCursor: team.repositories.page_info.end_cursor })
         end
       end
 
       if teams.page_info.has_next_page
-        teams_query({ 'teamCursor': teams.page_info.end_cursor })
+        teams_query({ teamCursor: teams.page_info.end_cursor })
       end
     end
   end
 
   def repos_query(vars = {})
-    result = Github::Client.query(Github::ReposQuery, variables: vars.merge({ 'org': ORG }))
+    result = query(Github::ReposQuery, vars)
     repos = result.data&.organization&.team&.repositories
     
     if repos.nil?
-      LOG.error("no such team: #{vars['teamSlug']}")
-      abort
+      LOG.info("repos nil for team: #{vars['teamSlug']}")
+      return
     end
     
-    repositories.edges.each do |edge|
-      repo = edge.node
-      LOG.info("  repo: #{repo.name} #{edge.permission}")
-      @nodes.add({ 'id': repo.name, 'group': 3 })
-      @links.push({ 'source': repo.name, 'target': vars['teamSlug'], 'label': edge.permission })
+    repos.edges.each do |edge|
+      unless edge.nil?
+        repo = edge.node
+        LOG.info("  repo: #{repo.name} #{edge.permission}")
+        @nodes.add({ 'id' => repo.name, 'group' => 3 })
+        @links.push({ 'source' => repo.name, 'target' => vars[:teamSlug], 'label' => edge.permission })
+      end
     end
 
-    if repositories.page_info.has_next_page
-      repos_query({ 'teamSlug': vars['teamSlug'], 'repoCursor': repositories.page_info.end_cursor })
+    if repos.page_info.has_next_page
+      repos_query({ teamSlug: vars[:teamSlug], repoCursor: repos.page_info.end_cursor })
     end
   end
 end
